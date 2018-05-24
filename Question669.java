09e6cf0eb1688e8662922338c4aa0e012377bd99
songyuanfang <songyuanfang@kuaishou.com>
2018-05-24 21:33:21 +0800

    5-24

---
+++ Question669.java
@@ -0,0 +1,15 @@
+public class Question669{
+     class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+            TreeNode(int x) { val = x; }
+        }
+    
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if(root == null)
+            return null;
+
+    }
+
+}
