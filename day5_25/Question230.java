44f5f881c652fe4fb60622da3384ca176500b406
songyuanfang <songyuanfang@kuaishou.com>
2018-05-25 21:10:45 +0800

    question230

---
+++ day5_25/Question230.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class Question230{
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){val = x;}
+    }
+
+    ArrayList<Integer> array = new ArrayList<>();
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    public int kthSmallest(TreeNode r, int k)
+    {
+            
+    }
+
+    private void helper(TreeNode r,)
+
+}
