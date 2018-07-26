ac86eac9236efde6f8c8d9890b589afbf221e659
songyuanfang <ysong243@wisc.edu>
2018-07-26 23:04:50 +0800

    今日最佳708,381,277

---
+++ day7_26/Question708.java
@@ -0,0 +1,35 @@
+public class Question708{
+    public Node insert(Node head, int insertVal) {
+        if(head == null){
+            Node node = new Node(insertVal, null);
+            node.next = node;
+            return node;
+        }
+
+        Node pre = head;
+        Node next = head.next;
+        while(next != null){
+            if(pre.val <= insertVal && next.val >= insertVal || next.val >= insertVal && next.val <= pre.val || next.val <= pre.val && pre.val <= insertVal){
+                Node node = new Node(insertVal, null);
+                pre.next = node;
+                node.next = next;
+                break;
+            }
+            pre = next;
+            next = next.next;
+        }
+        return head;
+    }
+}
+
+class Node {
+    public int val;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _next) {
+            val = _val;
+            next = _next;
+        }
+};
