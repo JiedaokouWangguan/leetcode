5d0cc51876595228a1c25c59c4d0cb89e93a3ead
Yuanfang Song <yuanfangsong@Yuanfangs-MacBook-Pro.local>
2018-10-12 11:39:29 -0500

    update

--- Tag_Quantcast/Question97.java
+++ Tag_Quantcast/Question97.java
@@ -25,10 +25,10 @@ public class Question97{
             map.put(key, false);
         return map.get(key);
     }
-
     public boolean isInterleave(String s1, String s2, String s3) {
         int lenS1 = s1.length();
         int lenS2 = s2.length();
+        if(lenS1 + lenS2 != s3.length()) return false;
         boolean[][] dp = new boolean[lenS1+1][lenS2+1];
         dp[0][0] = true;
         for(int i = 1;i<=lenS1;i++){
@@ -37,13 +37,16 @@ public class Question97{
         }
         for(int i = 1;i<=lenS2;i++){
             if(dp[0][i-1] && s2.charAt(i-1) == s3.charAt(i-1))
-                dp[0][0] = true;
+                dp[0][i] = true;
         }
         for(int i = 1;i<=lenS1;i++){
             for(int j = 1;j<=lenS2;j++){
+                if(i == 2 && j == 1){
+                    int s = 123;
+                }
                 if(s1.charAt(i-1) == s3.charAt(i-1 + j) && dp[i-1][j])
                     dp[i][j] = true;
-                if(s2.charAt(j-1) == s3.charAt(j-1 + j) && dp[i][j-1])
+                if(s2.charAt(j-1) == s3.charAt(j-1 + i) && dp[i][j-1])
                     dp[i][j] = true;
             }
         }
