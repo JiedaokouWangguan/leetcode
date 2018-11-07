3d34bc3d6f84b8ed1bdd418b7a36bb6c7ef54d15
Yuanfang Song <yuanfangsong@Yuanfangs-MacBook-Pro.local>
2018-11-07 15:38:40 -0600

    !

--- Tag_Google/Question739.java
+++ Tag_Google/Question739.java
@@ -2,21 +2,39 @@ import java.util.ArrayDeque;
 
 public class Question739{
     // 11.18 - 11.22, 5mins
-    public int[] dailyTemperatures(int[] temperatures) {
+    /*
+       public int[] dailyTemperatures(int[] temperatures) {
+       ArrayDeque<Integer> stack = new ArrayDeque<>();
+       int n = temperatures.length;
+       int[] result = new int[n];
+       for(int i = 0;i<n;i++){
+       int curTemp = temperatures[i];
+       while(stack.size() > 0 && curTemp > temperatures[stack.peek()]){
+       int tmpIndex = stack.pop();
+       result[tmpIndex] = i - tmpIndex;
+       }
+       stack.push(i);
+       }
+       while(stack.size() > 0){
+       result[stack.pop()] = 0;
+       }
+       return result;
+       }
+       */
+    public int[] dailyTemperatures(int[] T) {
         ArrayDeque<Integer> stack = new ArrayDeque<>();
-        int n = temperatures.length;
-        int[] result = new int[n];
-        for(int i = 0;i<n;i++){
-            int curTemp = temperatures[i];
-            while(stack.size() > 0 && curTemp > temperatures[stack.peek()]){
-                int tmpIndex = stack.pop();
-                result[tmpIndex] = i - tmpIndex;
+        int curIdx = T.length-1;
+        int[] result = new int[T.length];
+        while(curIdx >= 0){
+            int curTemp = T[curIdx];
+            while(stack.size() > 0 && curTemp >= T[stack.peek()]){
+                stack.pop();
             }
-            stack.push(i);
-        }
-        while(stack.size() > 0){
-            result[stack.pop()] = 0;
+            if(stack.size() == 0) result[curIdx] = 0;
+            else result[curIdx] = stack.peek() - curIdx;
+            stack.push(curIdx);
+            curIdx -= 1;
         }
         return result;
-    }
+    }   
 }
